ef7c2d11ac429ed6b1da8832a20554506125da00
Adamdad <787707188@qq.com>
2024-03-14 20:39:46 +0800

    Hash3D: Training-free Acceleration for 3D Generation

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# hash3D
+# Hash3D: Training-free Acceleration for 3D Generation
+We propose, Hash3D, a universal solution to acclerate score distillation sampling~(SDS) based 3D generation. By effectively hashing and reusing these feature maps across neighboring timesteps and camera angles, Hash3D substantially prevents redundant calculations, thus accelerating the diffusion model's inference in 3D generation tasks.
+
+
+## Project Structure
+The repository is organized into three main directories, each catering to a different aspect of Hash3D's application:
+
+1. `threesdtudio-hash3d`: Contains the implementation of Hash3D tailored for use with the (`threestudio`)[https://github.com/threestudio-project/threestudio] environment.
+2. `dreamgaussian-hash3d`: Focuses on integrating Hash3D with the DreamGaussian for image-to-3D generation.
+3. `gaussian-dreamer-hash3d`: Dedicated to applying Hash3D to GaussianDreamer for faster text-to-3D generative tasks.
+
+### What we add?
+The core mechanism is in the `guidance_loss` for each SDS loss computation. See `hash3D/threestudio-hash3d/threestudio/models/guidance/zero123_unified_guidance_cache.py` for example. The code for the hash table implementation is in `hash3D/threestudio-hash3d/threestudio/utils/hash_table.py`.
+
+## Getting Started
+
+### Installation
+Navigate to each of the specific directories for environment-specific installation instructions.
+
+### Usage
+Refer to the `README` within each directory for detailed usage instructions tailored to each environment.
+
+For example, to run Zero123+SDS with hash3D
+```shell
+cd threestudio-hash3d
+python launch.py --config configs/stable-zero123_hash3d.yaml --train --gpu 0 data.image_path=./load/images/dog1_rgba.png
+```
+
+### Evaliation
+1. **Image-to-3D**: GSO dataset GT meshes and renderings can be found online. With the rendering of the reconstructed 3D objects at `pred_dir` and the gt rendering at `gt_dir`, run
+
+```shell
+python eval_nvs.py --gt $gt_dir --pr $pred_dir 
+```
+2. **Text-to-3D**: Run all the prompts in `assets/prompt.txt`. And compute the CLIP score between text and rendered image as
+```shell
+python eval_clip_sim.py "$gt_prompt" $pred_dir --mode text
+```
+
---
+++ eval_clip_sim.py
@@ -0,0 +1,78 @@
+# eval the clip-similarity for an input image and a geneated images
+import os
+import cv2
+import torch
+import numpy as np
+from torchvision import transforms as T
+from transformers import CLIPModel, CLIPTokenizer, CLIPProcessor
+import tqdm
+
+# openai/clip-vit-large-patch14
+# openai/clip-vit-base-patch32
+# laion/CLIP-ViT-bigG-14-laion2B-39B-b160k
+class CLIP:
+    def __init__(self, device, model_name='openai/clip-vit-large-patch14'):
+        self.device = device
+        self.clip_model = CLIPModel.from_pretrained(model_name).to(self.device)
+        self.processor = CLIPProcessor.from_pretrained(model_name)
+
+    def encode_image(self, image):
+        pixel_values = self.processor(images=image, return_tensors="pt").pixel_values.to(self.device)
+        image_features = self.clip_model.get_image_features(pixel_values=pixel_values)
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)  # normalize features
+        return image_features
+
+    def encode_text(self, text):
+        inputs = self.processor(text=[text], padding=True, return_tensors="pt").to(self.device)
+        text_features = self.clip_model.get_text_features(**inputs)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)  # normalize features
+        return text_features
+    
+def sample_images(folder, num_images):
+    all_images = [f for f in os.listdir(folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    sampled = np.linspace(0, len(all_images) - 1, num_images, dtype=int)
+    return [all_images[i] for i in sampled]
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', type=str, help="path to front view image")
+    parser.add_argument('folder', type=str, help="path to folder containing rendered images")
+    parser.add_argument('--mode', type=str, choices=["image", "text"], default="image", help="mode to use for CLIP")
+    parser.add_argument('--num_images', type=int, default=8, help="number of images to sample from the folder")
+    parser.add_argument('--model_name', type=str, default='laion/CLIP-ViT-bigG-14-laion2B-39B-b160k', help="CLIP model name")
+    
+    opt = parser.parse_args()
+
+    # Initialize CLIP
+    clip = CLIP('cuda', model_name=opt.model_name)
+    if opt.mode == "image":
+        # Load reference image and encode as reference features
+        ref_img = cv2.imread(opt.image, cv2.IMREAD_COLOR)
+        ref_img = cv2.cvtColor(ref_img, cv2.COLOR_BGR2RGB)
+        with torch.no_grad():
+            ref_features = clip.encode_image(ref_img)
+    elif opt.mode == "text":
+        ref_text = opt.image
+        with torch.no_grad():
+            ref_features = clip.encode_text(ref_text)
+        
+    # Sample images from the folder
+    sampled_images = sample_images(opt.folder, opt.num_images)
+
+    # Iterate through sampled images
+    results = []
+    for filename in tqdm.tqdm(sampled_images):
+        image_path = os.path.join(opt.folder, filename)
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        with torch.no_grad():
+            cur_features = clip.encode_image(image)
+        similarity = (ref_features * cur_features).sum(dim=-1).mean().item()
+        results.append(similarity)
+    
+    # Compute the average similarity
+    avg_similarity = np.mean(results)
+    print(f'Average similarity: {avg_similarity}')
